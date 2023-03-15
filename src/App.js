40b12057709e18d7bf50428d8a179a543d778cb3
Shubham Shah <shubhamshahrising@gmail.com>
2023-03-15 09:26:52 +0530

    feat: control rendering of tabs and selection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Container, Navbar, Tab, Tabs } from "react-bootstrap";
+import { Container, Nav, Navbar, Tab, Tabs } from "react-bootstrap";
 
 import "./App.css";
-import FloatingModalButton from "./FloatingModalButton";
+import FloatingOverlayButton from "./FloatingOverlayButton";
 import showcaseData from "./showcaseData";
 
 function App() {
@@ -13,24 +13,40 @@ function App() {
         </Container>
       </Navbar>
       <Container className="mt-2 h-100">
-        <Tabs className="mb-3" mountOnEnter>
-          {showcaseData.demos.map(({ label, description, iframeLink }) => (
-            <Tab
-              className="h-100vh position-relative"
-              eventKey={label}
-              title={label}
-            >
-              <iframe
-                src={iframeLink}
-                style={{ width: "100%", height: "100%" }}
-                title="searchbox-voice-search"
-                allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-                sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-              ></iframe>
-              <FloatingModalButton title={label} description={description} />
-            </Tab>
-          ))}
-        </Tabs>
+        <Tab.Container
+          className="mb-3 visible-md hidden-sm"
+          mountOnEnter
+          defaultActiveKey={showcaseData.demos[0].label}
+        >
+          <Nav variant="tabs hidden-sm">
+            {showcaseData.demos.map(({ label }, idx) => (
+              <Nav.Item>
+                <Nav.Link eventKey={label}>{label}</Nav.Link>
+              </Nav.Item>
+            ))}
+          </Nav>
+          <Tab.Content className="mt-2">
+            {showcaseData.demos.map(({ label, description, iframeLink }) => (
+              <Tab.Pane
+                className="h-100vh position-relative"
+                eventKey={label}
+                title={label}
+              >
+                <iframe
+                  src={iframeLink}
+                  style={{ width: "100%", height: "100%" }}
+                  title="searchbox-voice-search"
+                  allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+                  sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+                ></iframe>
+                <FloatingOverlayButton
+                  title={label}
+                  description={description}
+                />
+              </Tab.Pane>
+            ))}
+          </Tab.Content>
+        </Tab.Container>
       </Container>
     </div>
   );

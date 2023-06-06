83d40e153b221d6d26313328eb6cf3464b24be2a
Shubham Shah <shubhamshahrising@gmail.com>
2023-06-06 10:28:36 +0530

    feat: select between react and vue showcase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
+import "./custom-select.css";
+
 const root = ReactDOM.createRoot(document.getElementById("search-showcase"));
 
-root.render(<App library={"vue"} />);
+const Main = () => {
+  const [library, setLibrary] = useState("react");
+  return (
+    <>
+      <div className="custom-dropdown">
+        <select
+          className="custom-dropdown__select custom-dropdown__select--white"
+          value={library}
+          onChange={(e) => setLibrary(e.target.value)}
+        >
+          <option value="react">&#x269b; React</option>
+          <option value="vue">&#x270c; Vue</option>
+        </select>
+      </div>
+      <App library={library} />
+    </>
+  );
+};
+
+root.render(<Main />);
+
+/* Use this file for testing showcase*/

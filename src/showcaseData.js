d1a1f4196964c51e443d0c24a05ec4e27280f17b
Shubham Shah <shubhamshahrising@gmail.com>
2023-07-03 10:11:36 +0530

    chore: update description information

--- src/showcaseData.js
+++ src/showcaseData.js
@@ -27,9 +27,12 @@ const showcaseData = {
         "https://codesandbox.io/embed/github/appbaseio/reactivesearch/tree/feat/react-showcase-examples/packages/web/examples/SearchBoxWithAutosuggestions?fontsize=14&hidenavigation=1&theme=dark&view=preview",
       description: (
         <div>
-          When <code>autosuggest</code> prop is set to <code>true</code>, the
-          SearchBox will query the index for suggestions based on the user's
-          input
+          When{" "}
+          <a href="https://docs.reactivesearch.io/docs/reactivesearch/react/search/searchbox/#autosuggest">
+            <code>autosuggest</code>
+          </a>{" "}
+          prop is set to <code>true</code>, the SearchBox will query the index
+          for suggestions based on the user's input
         </div>
       ),
     },
@@ -39,7 +42,11 @@ const showcaseData = {
         "https://codesandbox.io/embed/github/appbaseio/reactivesearch/tree/feat/react-showcase-examples/packages/web/examples/SearchBoxWithPopularRecentSuggestions?fontsize=14&hidenavigation=1&theme=dark&view=preview",
       description: (
         <div>
-          <b>Popular Suggestions:</b>
+          <b>
+            <a href="https://docs.reactivesearch.io/docs/reactivesearch/react/search/searchbox/#enablepopularsuggestions">
+              Popular Suggestions:
+            </a>
+          </b>
           <p>
             You can also enable popular suggestions, which show frequently
             searched terms. To do this, set the{" "}
@@ -47,7 +54,12 @@ const showcaseData = {
             customize the behavior of popular suggestions using the{" "}
             <code>popularSuggestionsConfig</code> prop.
           </p>
-          <b>Recent Suggestions:</b>
+          <b>
+            {" "}
+            <a href="https://docs.reactivesearch.io/docs/reactivesearch/react/search/searchbox/#enablerecentsuggestions">
+              Recent Suggestions:
+            </a>{" "}
+          </b>
           <p>
             You can also enable recent suggestions, which show the user's most
             recent searches. To do this, set the{" "}
@@ -189,7 +201,7 @@ const showcaseData = {
           useful for users on mobile devices or for those with accessibility
           needs. Read more about this prop and see the whole SearchBox component
           reference over{" "}
-          <a href="https://docs.reactivesearch.io/docs/reactivesearch/react/search/searchbox/#showvoicesearch">
+          <a href="https://docs.reactivesearch.io/docs/reactivesearch/vue/search/SearchBox/#showvoicesearch">
             here
           </a>
           .
@@ -202,9 +214,12 @@ const showcaseData = {
         "https://codesandbox.io/embed/github/appbaseio/reactivesearch/tree/feat/just-for-csb-001/packages/vue/examples/search-showcase/auto-suggestions?embed=1&file=src%2FApp.vue&hideExplorer=1&view=preview",
       description: (
         <div>
-          When <code>autosuggest</code> prop is set to <code>true</code>, the
-          SearchBox will query the index for suggestions based on the user's
-          input
+          When{" "}
+          <a href="https://docs.reactivesearch.io/docs/reactivesearch/vue/search/SearchBox/#autosuggest">
+            <code>autosuggest</code>
+          </a>{" "}
+          prop is set to <code>true</code>, the SearchBox will query the index
+          for suggestions based on the user's input
         </div>
       ),
     },
@@ -214,7 +229,11 @@ const showcaseData = {
         "https://codesandbox.io/embed/github/appbaseio/reactivesearch/tree/feat/just-for-csb-001/packages/vue/examples/search-showcase/simple-search?embed=1&file=src%2FApp.vue&hideExplorer=1&view=preview",
       description: (
         <div>
-          <b>Popular Suggestions:</b>
+          <b>
+            <a href="https://docs.reactivesearch.io/docs/reactivesearch/vue/search/SearchBox/#enablepopularsuggestions">
+              Popular Suggestions:
+            </a>
+          </b>
           <p>
             You can also enable popular suggestions, which show frequently
             searched terms. To do this, set the{" "}
@@ -222,7 +241,12 @@ const showcaseData = {
             customize the behavior of popular suggestions using the{" "}
             <code>popularSuggestionsConfig</code> prop.
           </p>
-          <b>Recent Suggestions:</b>
+          <b>
+            {" "}
+            <a href="https://docs.reactivesearch.io/docs/reactivesearch/vue/search/SearchBox/#enablerecentsuggestions">
+              Recent Suggestions:
+            </a>{" "}
+          </b>
           <p>
             You can also enable recent suggestions, which show the user's most
             recent searches. To do this, set the{" "}
@@ -240,8 +264,11 @@ const showcaseData = {
       description: (
         <div>
           <p>
-            This can be enabled by setting the <code>autosuggest</code> prop to{" "}
-            <code>false</code>.
+            This can be enabled by setting the{" "}
+            <a href="https://docs.reactivesearch.io/docs/reactivesearch/vue/search/SearchBox/#autosuggest">
+              <code>autosuggest</code>
+            </a>{" "}
+            prop to <code>false</code>.
           </p>
           <p>
             With instant search, users can quickly see which search terms are
@@ -264,8 +291,12 @@ const showcaseData = {
       description: (
         <div>
           <p>
-            The <code>focusShortcuts</code> prop allows you to define a list of
-            keyboard shortcuts that will focus the search box when pressed.
+            The{" "}
+            <a href="https://docs.reactivesearch.io/docs/reactivesearch/vue/search/SearchBox/#focusshortcuts">
+              <code>focusShortcuts</code>
+            </a>{" "}
+            prop allows you to define a list of keyboard shortcuts that will
+            focus the search box when pressed.
           </p>
           <p>
             For example, if you want to focus the search box when the user
@@ -291,7 +322,7 @@ const showcaseData = {
         <div>
           <p>
             A simple AI QnA app using{" "}
-            <a href="https://docs.reactivesearch.io/docs/reactivesearch/react/search/aianswer/">
+            <a href="https://docs.reactivesearch.io/docs/reactivesearch/vue/search/aianswer/">
               AIAnswer
             </a>{" "}
             component
@@ -314,7 +345,7 @@ const showcaseData = {
         <div>
           <p>
             A simple AI QnA app using{" "}
-            <a href="https://docs.reactivesearch.io/docs/reactivesearch/react/search/aianswer/">
+            <a href="https://docs.reactivesearch.io/docs/reactivesearch/vue/search/aianswer/">
               AIAnswer
             </a>{" "}
             component where you can also ask follow up questions.
@@ -337,7 +368,7 @@ const showcaseData = {
         <div>
           <p>
             Find your answers for the questions asked using{" "}
-            <a href="https://docs.reactivesearch.io/docs/reactivesearch/react/search/aianswer/">
+            <a href="https://docs.reactivesearch.io/docs/reactivesearch/vue/search/SearchBox/#enableai">
               SearchBox
             </a>{" "}
             component.

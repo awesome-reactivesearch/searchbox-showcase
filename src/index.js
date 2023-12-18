9d5c6bfdd63dd63052e051ae2690be89b9af1148
Siddharth Kothari <sids.aquarius@gmail.com>
2023-12-18 21:00:52 +0700

    format: support URL param to set specific view

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-
 import "./custom-select.css";
 
 const root = ReactDOM.createRoot(document.getElementById("search-showcase"));
 
 const Main = () => {
+  // Use useState to manage the library state
   const [library, setLibrary] = useState("react");
+
+  // useEffect to parse URL on component mount
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const libraryParam = queryParams.get("library");
+    if (libraryParam === "vue" || libraryParam === "react") {
+      setLibrary(libraryParam);
+    }
+  }, []);
+
+  // Function to handle library change and update URL
+  const handleLibraryChange = (e) => {
+    const newLibrary = e.target.value;
+    setLibrary(newLibrary);
+    window.history.pushState({}, '', `?library=${newLibrary}`);
+  };
+
   return (
     <>
       <div className="custom-dropdown">
         <select
           className="custom-dropdown__select custom-dropdown__select--white"
           value={library}
-          onChange={(e) => setLibrary(e.target.value)}
+          onChange={handleLibraryChange}
         >
           <option value="react">&#x269b; React</option>
           <option value="vue">&#x270c; Vue</option>
@@ -27,5 +44,3 @@ const Main = () => {
 };
 
 root.render(<Main />);
-
-/* Use this file for testing showcase*/
